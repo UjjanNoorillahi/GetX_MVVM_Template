9fb0cee2c4c671b7c1dc0baaef58ae89bc39fd0f
Noor Illahi <ujjannoorillahi@gmail.com>
2024-07-31 00:12:50 +0500

    components

--- lib/res/colors/colors.dart
+++ lib/res/colors/colors.dart
@@ -1,27 +1,19 @@
-import 'package:flutter/material.dart';
+import 'dart:ui';
 
 class AppColor {
-  // static const String themeColor1CodeHexa = "#fff1a443";
-  // static const String themeColor2CodeHexa = "#ff2e4757";
-  // static const String appbarColorCodeHexa = "#fff7d2a0";
 
-  static const blackColor = Colors.black;
-  static const whiteColor = Colors.white;
-  static const greyColor = Colors.grey;
+  static const Color blackColor = Color(0xff000000) ;
+  static const Color whiteColor = Color(0xffffffff) ;
 
-  static const appbarColor = Color(0xfff7d2a0);
-  static const primaryColor = Color(0xfff1a443);
-  static const secondaryColor = Color(0xff2e4757);
+  static const Color primaryColor =  Color(0xff01B1C9);
 
-  // Color themeColor1 =
-  // Color(int.parse(themeColor1CodeHexa.replaceAll('#', '0x')));
-  // Color themeColor2 =
-  // Color(int.parse(themeColor2CodeHexa.replaceAll('#', '0x')));
-  // Color appbarColor =
-  // Color(int.parse(appbarColorCodeHexa.replaceAll("#", "0x")));
-  static BoxShadow cardShadow = BoxShadow(
-      color: Colors.grey.withOpacity(0.6),
-      spreadRadius: 0.5,
-      blurRadius: 2,
-      offset: const Offset(0, 5));
+  static const Color primaryButtonColor =  Color(0xff01B1C9);
+  static const Color secondaryButtonColor =  Color(0xffFC3F5B);
+
+  static const Color redColor =  Color(0xffFC3F5B);
+
+  static const Color primaryTextColor =  Color(0xff000000);
+  static const Color secondaryTextColor =  Color(0xff444648);
+
+
 }
